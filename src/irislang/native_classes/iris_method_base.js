3836a609af8a4f9cf5b7a6317d6f2a36ddd80756
CodeDaraW <CodeDaraW@gmail.com>
2017-01-28 16:34:21 +0800

    Complete native class iris method base

---
+++ src/irislang/native_classes/iris_method_base.js
@@ -0,0 +1,55 @@
+import { IrisDev } from '../util';
+import { IrisMethod } from "../core";
+import {
+    method_object_sym
+    } from "../util/iris_symbol";
+
+export default class IrisMethodBase {
+
+    native_class_name_define() {
+        return "Method";
+    }
+
+    native_super_class_define() {
+        return return IrisDev.get_class("Object");;
+    }
+
+    native_super_module_define() {
+        return null;
+    }
+
+    native_alloc() {
+        return new IrisMethodBaseTag();
+    }
+
+    native_class_define(class_obj) {
+        
+    }
+
+    static get_name(self, parameter_list, variable_parameter_list, context, thread_info) {
+        return IrisDev.create_string(IrisDev.get_native_object_ref(self).method_name);
+    }
+
+    static get IrisMethodBaseTag() {
+        return IrisMethodBaseTag;
+    }
+
+}
+
+class IrisMethodBaseTag {
+    constructor() {
+        this[method_object_sym] = null;
+    }
+
+    get method_name() {
+        return this[method_object_sym].method_name;
+    }
+
+    get method_obj() {
+        return this[method_object_sym];
+    }
+
+    set method_obj(method_obj) {
+        this[method_object_sym] = method_obj;
+    }
+}
