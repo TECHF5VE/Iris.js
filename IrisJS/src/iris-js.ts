4c23f1df7441ca461aac82608545b819795e3c60
ChiWin <contractswif@hotmail.com>
2017-03-13 17:17:30 +0800

    Support for Visual Studio 2017

---
+++ IrisJS/src/iris-js.ts
@@ -0,0 +1,10 @@
+// import { method } from './iris_method'
+
+export default class IrisClass {
+     constructor() {
+         console.log("Hello Iris")
+     }
+}
+
+ const dummy: IrisClass = new IrisClass()
+// method
