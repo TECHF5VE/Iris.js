44d3824ed534e71a6afe81a0a6ade591b1b76881
Chi <contractswif@hotmail.com>
2017-03-27 18:48:38 +0800

    fix(test): fix test

--- test/iris-js.test.ts
+++ test/iris-js.test.ts
@@ -7,8 +7,4 @@ describe('Dummy test', () => {
   it('works if true is truthy', () => {
     expect(true).toBeTruthy()
   })
-
-  it('DummyClass is instantiable', () => {
-    expect(new DummyClass()).toBeInstanceOf(DummyClass)
-  })
 })
